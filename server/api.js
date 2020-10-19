52f6fadeb7551b64c3e6460d852e32fd83bfe404
Buchraatkeh1984 <61603626+Buchraatkeh1984@users.noreply.github.com>
2020-10-19 18:49:04 +0100

    heroku test

--- server/api.js
+++ server/api.js
@@ -14,5 +14,16 @@ router.get("/", (_, res, next) => {
 		res.json({ message: "Hello, world!" });
 	});
 });
+router.get("/graduates", (_, res, next) => {
+	
+	Connection.connect((err) => {
+		if (err) {
+			return next(err);
+		}
+		Connection.query('SELECT * FROM graduates', (error, result) => {
+				res.json(result.rows);
+			})
+	});
+});
 
 export default router;
