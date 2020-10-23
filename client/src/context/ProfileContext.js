901795df336386bc9a91a549970b125292b156f8
OrhanBakir <53667329+OrhanBakir@users.noreply.github.com>
2020-10-24 00:29:42 +0100

    fizzz

--- client/src/context/ProfileContext.js
+++ client/src/context/ProfileContext.js
@@ -65,11 +65,11 @@ const ProfileState = (props) =>{
 
 
 	const getProfile =  (id) => {
+		console.log('get profile called')
 		dispatch({ type: types.Set_Is_Loading });
 		axios.get(`https://gist.githubusercontent.com/OBakir90/46c0de835cb3db4c42f655e5f467825a/raw/d16c488a33cc1ebbceea866fe988591c3683bf0c/myprofile.json`)
-			.then(response=>response.json)
-			.then((data)=>{
-				console.log(data);
+			.then((response)=>{
+				console.log('response', response);
 				dispatch({ type: types.Set_Profile, payload:data });
 			})
 			.catch((error)=>{
