4bf6d7e2d426aa74a3e47863d9ddd1fafab758f1
OrhanBakir <53667329+OrhanBakir@users.noreply.github.com>
2020-10-26 11:12:04 +0000

    test

--- client/src/context/ProfileContext.js
+++ client/src/context/ProfileContext.js
@@ -74,8 +74,12 @@ const ProfileState = (props) =>{
 		dispatch({ type: types.Set_Is_Loading });
 		axios.get(`${baseUrl}/graduates/${id}`)
 			.then((response)=>{
+				dispatch({ type: types.Set_All_Profiles, payload:response.data });
+			})
+			.then((response)=>{
 				console.log('response data', response.data[0]);
-				const graduate = response.data[0]
+				const graduate = response.data[0];
+				console.log('graduate', graduate, types.Set_User, types.Set_Profile);
 				dispatch({ type: types.Set_User, payload:graduate });
 				// dispatch({ type:types.Set_Profile, payload: response.data[0] });
 			})
