f5105d00aadb9ef8516dc09f27c54ee1db3b5e4f
Iman Feisali <iman.feisali@gmail.com>
2020-10-29 21:52:18 +0000

    .env created

--- server/db.js
+++ server/db.js
@@ -17,11 +17,11 @@ if (process.env.DATABASE_URL) {
   //default local config
   config = {
 
-    user: 'nawal',
+    user: process.env.db_user,
     host: "localhost",
     database: "cyf_knowledge_checklist",
-    password: 123456,
-    port: 5433,
+    password: process.env.db_password,
+    port: process.env.db_port,
   };
 
 }
