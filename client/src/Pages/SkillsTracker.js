da62d24d110f1f629b2c05e09b2f263e12601749
Altom <Altomhussain2001@yahoo.com>
2020-10-31 18:16:20 +0000

    Fetching and setting ability

--- client/src/Pages/SkillsTracker.js
+++ client/src/Pages/SkillsTracker.js
@@ -2,63 +2,92 @@ import React, { useEffect, useState } from "react";
 import fakeData from "../fakeData.json";
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 
-
 import BtnContainer from "../components/BtnContainer";
 
-function getAbility(achievements,  skill, objective){
-  const matchingAbility = achievements.filter((ability)=>(ability.skill===skill && ability.objective===objective))
-  return matchingAbility.length? matchingAbility[0].ability: null
+function getAbility(achievements, skill, objective) {
+  const matchingAbility = achievements.filter(
+    (ability) => ability.skill === skill && ability.objective === objective
+  );
+  return matchingAbility.length ? matchingAbility[0].ability : null;
 }
-export default function Html({skill}) {
-  const [xyz, setXyz] = useState([])
+export default function Html({ skill }) {
+  const [xyz, setXyz] = useState([]);
   console.log();
-  
-  useEffect(() => {
+  const fetchLearningObj = () => {
+    fetch(`/api/learningobjectives/${localStorage.getItem("user")}/${skill}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          throw data;
+        }
+        setXyz(data);
+      });
+  };
+  useEffect(fetchLearningObj, [skill]);
+  //fetch here call
 
-fetch(`/api/learningobjectives/${localStorage.getItem('user')}/${skill}`)
-.then((response) => response.json())
-        .then((data) => {
-          if (data.error) {
-            throw data;
-          }
-          setXyz(data)
-        })
-      },[skill])
- //fetch here call 
+  const [achievements, setAchievements] = useState([]);
 
-const [achievements, setAchievements] = useState([])
-
- function updateAchievement(ability,  skill, objective){
- 
-  const newAchievement=  
- achievements.filter(achievement =>!( achievement.skill===skill && achievement.objective===objective))
- .concat({
-       skill,
-       objective,
-       ability
+  function updateAchievement(newAbility, id) {
+    fetch(`/api/ability`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        token: window.localStorage.getItem("token"),
+      },
+      body: JSON.stringify({
+        ability: newAbility,
+        learning_obj_id: id,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          throw new Error(data.error);
+        }
       })
-    setAchievements(newAchievement )
+      .then(fetchLearningObj);
+    setXyz(
+      xyz.map((obj) => {
+        if (obj.id === id) {
+          return { ...obj, ability: newAbility };
+        }
+        return obj;
+      })
+    );
+
+    //   const newAchievement=
+    //  achievements.filter(achievement =>!( achievement.skill===skill && achievement.objective===objective))
+    //  .concat({
+    //        skill,
+    //        objective,
+    //        ability
+    //       })
+    //     setAchievements(newAchievement )
   }
-  console.log(achievements)
-  
-//const skill = "css";
+  console.log(achievements);
+
+  //const skill = "css";
   return (
     <div className="learning-objective-container">
       <h2>{skill}</h2>
       <ul>
-        {xyz.map(({description}, index) => {
-        const ability =   getAbility(achievements, skill, description)
-        function updateAbility(newAbility){
-          updateAchievement( newAbility, skill, description)
-        }
-        console.log(ability)
-          return <li key={index}>{description}
+        {xyz.map(({ description, id, ability }, index) => {
+          // const ability =   getAbility(achievements, skill, description)
+          function updateAbility(newAbility) {
+            updateAchievement(newAbility, id);
+          }
+          return (
+            <li key={index}>
+              {description}
 
-          <BtnContainer ability = {ability} updateAbility={updateAbility} />
-         
-
-  
-          </li>;
+              <BtnContainer
+                ability={ability}
+                updateAbility={updateAbility}
+                learningObjId={id}
+              />
+            </li>
+          );
         })}
       </ul>
     </div>
