4b3b2fe7254cfc99400845127ac63ec65b9b73b3
Altom <Altomhussain2001@yahoo.com>
2020-11-04 22:24:36 +0000

    Added back link to mentorsview and edit to edit learnig objectives by mentor

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Header() {
+export default function Header({ editLoImg, back }) {
   return (
     <div className="header">
       <img
@@ -8,6 +8,10 @@ export default function Header() {
         alt="code your future"
         className="header-img"
       ></img>
+      <a href="/MentorsView">
+        <h3>{back}</h3>
+      </a>
+      <a href="/mentorsedit">{editLoImg}</a>
       <a href="/">
         <img
           src="https://www.flaticon.com/svg/static/icons/svg/159/159707.svg"
