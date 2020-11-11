b1558a0a7d56c5dddecb146cf618a1c22f7c6e01
Iman Feisali <iman.feisali@gmail.com>
2020-11-11 02:37:12 +0000

    picture added in signup page

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -70,118 +70,131 @@ const SignupForm = () => {
   return (
     <div>
       <Header />
-      {hasRegistered ? (
-        <Modal role={input.userRole} />
-      ) : (
-        <form onSubmit={handleSubmit}>
-          <div className="sign-form">
-            <label for="firstName">Name</label>
-            <input
-              type="text"
-              placeholder="Name"
-              value={input.firstName}
-              onChange={handleChange}
-              name="firstName"
-            />
-            {errors.firstName && <p className="error">*{errors.firstName} </p>}
-            <label for="lastName">Surname</label>
-            <input
-              type="text"
-              placeholder="Surname"
-              value={input.lastName}
-              onChange={handleChange}
-              name="lastName"
-            />
-            {errors.lastName && <p className="error">*{errors.lastName} </p>}
-            <label for="userEmail">Email</label>
-            <input
-              type="userEmail"
-              placeholder="Email"
-              value={input.userEmail}
-              onChange={handleChange}
-              name="userEmail"
-            />
-            {errors.userEmail && <p className="error">*{errors.userEmail} </p>}
-            <label for="userPassword">Password</label>
-            <input
-              type="password"
-              placeholder="Password"
-              value={input.userPassword}
-              onChange={handleChange}
-              name="userPassword"
-            />
-            {errors.userPassword && (
-              <p className="error">*{errors.userPassword} </p>
-            )}
-            <label for="">Confirm Password</label>
-            <input
-              type="password"
-              placeholder="Confirm Password"
-              value={input.confirmPassword}
-              onChange={handleChange}
-              name="confirmPassword"
-            />
-            {errors.confirmPassword && (
-              <p className="error">*{errors.confirmPassword} </p>
-            )}
-            <label for="cyfCity">City</label>
-            <input
-              type="text"
-              placeholder="City"
-              value={input.cyfCity}
-              onChange={handleChange}
-              name="cyfCity"
-            />
-            {errors.cyfCity && <p className="error">*{errors.cyfCity} </p>}
-            <label for="userClassId">Class</label>
-            <input
-              type="number"
-              placeholder="Class"
-              value={input.userClassId}
-              onChange={handleChange}
-              name="userClassId"
-            />
-            {errors.userClassId && (
-              <p className="error">*{errors.userClassId} </p>
-            )}
-            <label for="userGithub">Github Name</label>
-            <input
-              type="text"
-              placeholder="Github Name"
-              value={input.userGithub}
-              onChange={handleChange}
-              name="userGithub"
-            />
-            <label for="userSlack">Slack Handler</label>
-            <input
-              type="text"
-              placeholder="Slack Handler"
-              value={input.userSlack}
-              onChange={handleChange}
-              name="userSlack"
-            />
+      <div className="signup-container">
+        <div className="signup-img-container">
+          <img
+            src="https://i.ibb.co/hVRjyML/cyf-001.jpg"
+            alt="code your future"
+            border="0"
+            className="signup-image"
+          ></img>
+        </div>
+        {hasRegistered ? (
+          <Modal role={input.userRole} />
+        ) : (
+          <form onSubmit={handleSubmit} className="signup-form-container">
+            <div className="sign-form">
+              <label for="firstName">Name</label>
+              <input
+                type="text"
+                placeholder="Name"
+                value={input.firstName}
+                onChange={handleChange}
+                name="firstName"
+              />
+              {errors.firstName && (
+                <p className="error">*{errors.firstName} </p>
+              )}
+              <label for="lastName">Surname</label>
+              <input
+                type="text"
+                placeholder="Surname"
+                value={input.lastName}
+                onChange={handleChange}
+                name="lastName"
+              />
+              {errors.lastName && <p className="error">*{errors.lastName} </p>}
+              <label for="userEmail">Email</label>
+              <input
+                type="userEmail"
+                placeholder="Email"
+                value={input.userEmail}
+                onChange={handleChange}
+                name="userEmail"
+              />
+              {errors.userEmail && (
+                <p className="error">*{errors.userEmail} </p>
+              )}
+              <label for="userPassword">Password</label>
+              <input
+                type="password"
+                placeholder="Password"
+                value={input.userPassword}
+                onChange={handleChange}
+                name="userPassword"
+              />
+              {errors.userPassword && (
+                <p className="error">*{errors.userPassword} </p>
+              )}
+              <label for="">Confirm Password</label>
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                value={input.confirmPassword}
+                onChange={handleChange}
+                name="confirmPassword"
+              />
+              {errors.confirmPassword && (
+                <p className="error">*{errors.confirmPassword} </p>
+              )}
+              <label for="cyfCity">City</label>
+              <input
+                type="text"
+                placeholder="City"
+                value={input.cyfCity}
+                onChange={handleChange}
+                name="cyfCity"
+              />
+              {errors.cyfCity && <p className="error">*{errors.cyfCity} </p>}
+              <label for="userClassId">Class</label>
+              <input
+                type="number"
+                placeholder="Class"
+                value={input.userClassId}
+                onChange={handleChange}
+                name="userClassId"
+              />
+              {errors.userClassId && (
+                <p className="error">*{errors.userClassId} </p>
+              )}
+              <label for="userGithub">Github Name</label>
+              <input
+                type="text"
+                placeholder="Github Name"
+                value={input.userGithub}
+                onChange={handleChange}
+                name="userGithub"
+              />
+              <label for="userSlack">Slack Handler</label>
+              <input
+                type="text"
+                placeholder="Slack Handler"
+                value={input.userSlack}
+                onChange={handleChange}
+                name="userSlack"
+              />
 
-            <label for="userRole">Please select a role</label>
-            <select name="userRole" onChange={handleChange}>
-              <option value="select">Select</option>
-              <option value="Student">Student</option>
-              <option value="Mentor">Mentor</option>
-            </select>
-            {errors.userRole && <p className="error">*{errors.userRole} </p>}
-            <button
-              to="/modal"
-              className="signup-submit"
-              type="submit"
-              value="Submit"
-              className="submit"
-            >
-              Submit
-            </button>
-            <p className="error">{serverError}</p>
-          </div>
-        </form>
-      )}
-
+              <label for="userRole">Please select a role</label>
+              <select name="userRole" onChange={handleChange}>
+                <option value="select">Select</option>
+                <option value="Student">Student</option>
+                <option value="Mentor">Mentor</option>
+              </select>
+              {errors.userRole && <p className="error">*{errors.userRole} </p>}
+              <button
+                to="/modal"
+                className="signup-submit"
+                type="submit"
+                value="Submit"
+                className="submit"
+              >
+                Submit
+              </button>
+              <p className="error">{serverError}</p>
+            </div>
+          </form>
+        )}
+      </div>
       <div className="sign-up-footer">
         <Footer />
       </div>
